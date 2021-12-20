222444f8cadfa25d6154bacc6be41559d62dfd67
Creotip <creotip@gmail.com>
2021-12-20 15:23:32 +0200

    emotion cache provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,22 +4,36 @@ import Head from 'next/head'
 import { DefaultSeo } from 'next-seo'
 import { getSeo } from 'lib/getSeo'
 import customTheme from 'configs/theme'
+import { EmotionCache } from '@emotion/cache'
+import { CacheProvider } from '@emotion/react'
+import createEmotionCache from 'lib/createEmotionCache'
 
-function MyApp({ Component, pageProps }: AppProps) {
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+const clientSideEmotionCache = createEmotionCache()
+
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: MyAppProps) {
   const seo = getSeo()
   return (
-    <>
-      <Head>
-        <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon.png" />
-        <meta name="theme-color" content="#319795" />
-      </Head>
-      <DefaultSeo {...seo} />
+    <CacheProvider value={emotionCache}>
       <ChakraProvider theme={customTheme}>
+        <Head>
+          <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/favicon.png" />
+          <meta name="theme-color" content="#319795" />
+        </Head>
+        <DefaultSeo {...seo} />
+
         <Component {...pageProps} />
       </ChakraProvider>
-    </>
+    </CacheProvider>
   )
 }
 
