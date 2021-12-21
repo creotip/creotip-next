7db82959cfd2b2df5dd8c8577301b5675b55d145
Creotip <creotip@gmail.com>
2021-12-21 15:29:11 +0200

    site change

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const NextSitemapConfig = {
-  siteUrl: 'https://creotip-next.vercel.app/',
+  siteUrl: 'https://creotip.io',
   generateRobotsTxt: true,
 }
 
