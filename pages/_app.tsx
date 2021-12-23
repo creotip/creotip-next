b677b9e6b4716732cbecbe7e6cccbb36f781fac6
Creotip <creotip@gmail.com>
2021-12-23 12:46:11 +0200

    # added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,9 @@ import customTheme from 'configs/theme'
 import { EmotionCache } from '@emotion/cache'
 import { CacheProvider } from '@emotion/react'
 import createEmotionCache from 'lib/createEmotionCache'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import useAckee from 'use-ackee'
 
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache
@@ -21,6 +24,30 @@ function MyApp({
 }: MyAppProps) {
   const seo = getSeo()
   const mode = useColorModeValue('light', 'dark')
+  const router = useRouter()
+
+  const handleRouteChange = (url: any) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useAckee(
+      url,
+      {
+        server: 'https://ackee-creotip.vercel.app/',
+        domainId: 'ea31e3cd-6c5a-48a4-b290-99b4f734950e',
+      },
+      {
+        detailed: true,
+        ignoreLocalhost: true,
+        ignoreOwnVisits: true,
+      }
+    )
+  }
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
     <CacheProvider value={emotionCache}>
       <ChakraProvider theme={customTheme}>
