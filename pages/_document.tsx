f36ab11f53a44e082889a3a8c83ce2da79d4f94d
Creotip <creotip@gmail.com>
2022-01-31 00:54:53 +0200

    # added google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
+        <Head></Head>
         <body>
           <ColorModeScript />
           <Main />
